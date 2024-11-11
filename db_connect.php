2f4ff6d08859dd0b5ffeba7aaab45205b906bb40
Taehillah <ishmaelmafole@gmail.com>
2024-11-11 10:27:28 +0200

    remove output on the db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -8,7 +8,7 @@ $pass = 'Ishmael@12345';
 try {
     $pdo = new PDO("sqlsrv:server=$host;Database=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected to Azure SQL Database successfully!";
+ //   echo "Connected to Azure SQL Database successfully!";
 } catch (PDOException $e) {
     die("Could not connect to the database $db :" . $e->getMessage());
 }
