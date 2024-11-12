0181405acffb3f8314f88229910bc782e8b40c3c
Taehillah <ishmaelmafole@gmail.com>
2024-11-12 14:17:59 +0200

    js for charts.js

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -84,5 +84,7 @@ try {
 
     <!-- Bootstrap JS for hamburger functionality -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 </body>
 </html>
