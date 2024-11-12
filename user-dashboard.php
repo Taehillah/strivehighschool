3cd871536abdf4874dc2967d217ca32c48bb3b27
Taehillah <ishmaelmafole@gmail.com>
2024-11-12 19:30:43 +0200

    graph and gauges

--- user-dashboard.php
+++ user-dashboard.php
@@ -71,7 +71,7 @@ try {
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Logout</a></li>
                 </ul>
             </div>
         </div>
